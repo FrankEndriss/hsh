caaa41970a1cbaf7bdd655cb86a126dc6e1a28f7
Frank Endriss <fendriss@fum.de>
2015-04-17 10:50:52 +0200

    INitialisation of Executors extended

--- hsh/src/main/java/com/happypeople/hsh/hsh/DelegatingHshExecutor.java
+++ hsh/src/main/java/com/happypeople/hsh/hsh/DelegatingHshExecutor.java
@@ -1,20 +1,32 @@
-package com.happypeople.hsh;
+package com.happypeople.hsh.hsh;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import com.happypeople.hsh.HshContext;
+import com.happypeople.hsh.HshExecutor;
+import com.happypeople.hsh.HshRedirection;
 
 /** This class acts like a single Executor, but delegates the calls to a list of executors.
  */
 public class DelegatingHshExecutor implements HshExecutor {
 
-	private final List<HshExecutor> delegates=new ArrayList<HshExecutor>();
+	private final Iterable<HshExecutor> delegates;
+
+	/** Call this constructor with a List of HshExecutor objects, the iterable will be used in all methods to loop
+	 * over the delegates.
+	 * @param delegatesProvider List (or similar) of delegates
+	 */
+	public DelegatingHshExecutor(final Iterable<HshExecutor> delegatesProvider) {
+		this.delegates=delegatesProvider;
+	}
 
 	@Override
 	public int execute(final String[] command, final HshContext context, final List<HshRedirection> redirections) throws Exception {
 		for(final HshExecutor delegate : delegates)
 			if(delegate.canExecute(command))
 				return delegate.execute(command, context, redirections);
-		return -1;
+		throw new IllegalArgumentException("no provided executor can execute command: "+Arrays.asList(command));
 	}
 
 	@Override
