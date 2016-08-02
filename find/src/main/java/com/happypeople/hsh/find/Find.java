af9803f81eda1d2f6b37af8e058e1697c8bf0312
Frank Endriss <fendriss@fum.de>
2016-08-02 16:16:34 +0200

    Cleaned up Find.java

--- find/src/main/java/com/happypeople/hsh/find/Find.java
+++ find/src/main/java/com/happypeople/hsh/find/Find.java
@@ -15,94 +15,108 @@ import java.util.regex.Pattern;
 
 /** find implementation */
 public class Find {
-	public static void main(final String[] args) {
+    public static void main(final String[] args) {
 
-		final List<String> pathList=new ArrayList<String>();
-		final List<Expr> exprList=new ArrayList<Expr>();
+        final List<String> pathList = new ArrayList<>();
+        final List<Expr> exprList = new ArrayList<>();
 
-		for(int i=1; i<args.length; i++) {
-			if("-name".equals(args[i])) {
-				exprList.add(new NameExpr(args[i+1]));
-				break;
-			}
-			else
-				pathList.add(args[i]);
-		}
-		if(pathList.size()==0)
-			pathList.add(".");
+        for (int i = 1; i < args.length; i++) {
+            if ("-name".equals(args[i])) {
+                exprList.add(new NameExpr(args[i + 1]));
+                break;
+            } else
+                pathList.add(args[i]);
+        }
+        if (pathList.size() == 0)
+            pathList.add(".");
 
-		for(final String path : pathList)
-			try {
-				runRecursive(new File(path), exprList);
-			} catch (final IOException e) {
-				e.printStackTrace();
-			}
-	}
+        for (final String path : pathList)
+            try {
+                runRecursive(new File(path), exprList);
+            } catch (final IOException e) {
+                e.printStackTrace();
+            }
+    }
 
-	private static void runRecursive(final File file, final List<Expr> exprList) throws IOException {
-		if(!file.exists())
-			return;
+    private static void runRecursive(final File file, final List<Expr> exprList)
+        throws IOException {
+        if (!file.exists())
+            return;
 
-		final Path fpath=file.toPath();
+        final Path fpath = file.toPath();
 
-		/*
-		if(checkExpressions(file, exprList))
-			System.out.println(fpath);
-			*/
+        /*
+         * if(checkExpressions(file, exprList)) System.out.println(fpath);
+         */
 
-		//System.out.println("call walkFileTree, file="+file+" path: "+file.toPath());
-		Files.walkFileTree(fpath, EnumSet.noneOf(FileVisitOption.class),  Integer.MAX_VALUE, new FileVisitor<Path>() {
+        // System.out.println("call walkFileTree, file="+file+" path:
+        // "+file.toPath());
+        Files.walkFileTree(
+            fpath, EnumSet.noneOf(FileVisitOption.class), Integer.MAX_VALUE,
+            new FileVisitor<Path>() {
 
-			public FileVisitResult preVisitDirectory(final Path path, final BasicFileAttributes attrs) throws IOException {
-				if(checkExpressions(path.toFile(), exprList))
-					System.out.println(""+path);
-				return FileVisitResult.CONTINUE;
-			}
+                @Override
+                public FileVisitResult preVisitDirectory(final Path path,
+                    final BasicFileAttributes attrs) throws IOException {
+                    if (checkExpressions(path.toFile(), exprList))
+                        System.out.println("" + path);
+                    return FileVisitResult.CONTINUE;
+                }
 
-			public FileVisitResult visitFile(final Path path, final BasicFileAttributes attrs) throws IOException {
-				if(checkExpressions(path.toFile(), exprList))
-					System.out.println(""+path);
-				return FileVisitResult.CONTINUE;
-			}
+                @Override
+                public FileVisitResult visitFile(final Path path,
+                    final BasicFileAttributes attrs) throws IOException {
+                    if (checkExpressions(path.toFile(), exprList))
+                        System.out.println("" + path);
+                    return FileVisitResult.CONTINUE;
+                }
 
-			public FileVisitResult visitFileFailed(final Path path, final IOException exc)
-					throws IOException {
-				return FileVisitResult.CONTINUE;
-			}
+                @Override
+                public FileVisitResult visitFileFailed(final Path path,
+                    final IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
 
-			public FileVisitResult postVisitDirectory(final Path path, final IOException exc)
-					throws IOException {
-				return FileVisitResult.CONTINUE;
-			}
+                @Override
+                public FileVisitResult postVisitDirectory(final Path path,
+                    final IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
 
-		});
-	}
+            }
+        );
+    }
 
-	private static boolean checkExpressions(final File file, final List<Expr> exprList) {
-		for(final Expr expr : exprList)
-			if(!expr.isMatch(file))
-				return false;
-		return true;
-	}
+    private static boolean checkExpressions(final File file,
+        final List<Expr> exprList) {
+        for (final Expr expr : exprList)
+            if (!expr.isMatch(file))
+                return false;
+        return true;
+    }
 
+    public static void usage() {
+        System.err.println(
+            "usage: " + Find.class.getName() + " <pathlist> [expression]"
+        );
+        System.err.println("implemented expressions: -name <pattern>");
+    }
 
-	public static void usage() {
-		System.err.println("usage: "+Find.class.getName()+" <pathlist> [expression]");
-		System.err.println("implemented expressions: -name <pattern>");
-	}
+    private static interface Expr {
+        public boolean isMatch(File file);
+    }
 
-	private static interface Expr {
-		public boolean isMatch(File file);
-	}
+    private static class NameExpr implements Expr {
+        private final Pattern pattern;
 
-	private static class NameExpr implements Expr {
-		private final Pattern pattern;
-		public NameExpr(final String patternStr) {
-			this.pattern=Pattern.compile(patternStr);
-		}
-		public boolean isMatch(final File file) {
-			return pattern.matcher(file.getName()).matches();
-		}
+        public NameExpr(final String patternStr) {
+            this.pattern = Pattern.compile(patternStr);
+        }
 
-	}
+        @Override
+        public boolean isMatch(final File file) {
+            return this.pattern.matcher(file.getName()).matches();
+        }
+
+    }
 }
